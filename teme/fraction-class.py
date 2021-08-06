61de6c70638da4dde9fbd95b01a2fb92c2e22e29
Alexandru Florea <alexandru.floreaf1@gmail.com>
2021-08-06 16:25:57 +0300

    Tema curs 6

--- teme/fraction-class.py
+++ teme/fraction-class.py
@@ -23,6 +23,20 @@ class Fraction:
 
         return Fraction(new_num // common_divisor, new_denom // common_divisor)
 
+    def __mul__(self, other):
+        new_num = self.num * other.num
+        new_denom = self.denom * other.denom
+        common_divisor = gcd(new_num, new_denom)
+
+        return Fraction(new_num // common_divisor, new_denom // common_divisor)
+
+    def divide(self, other):
+        new_num = self.num * other.denom
+        new_denom = self.denom * other.num
+        common_divisor = gcd(new_num, new_denom)
+
+        return Fraction(new_num // common_divisor, new_denom // common_divisor)
+
     def inverse(self):
         return f"{self.denom}/{self.num}"
 
@@ -32,5 +46,7 @@ my_f2 = Fraction(4, 8)
 my_f3 = Fraction(1, 2)
 
 print(my_f1 + my_f2 + my_f3)
-print(my_f1 - my_f2)
+print(my_f1 - my_f2 + my_f3)
+print(my_f1 * my_f2 + my_f3)
+print(my_f1.divide(my_f2))
 print(my_f3.inverse())
